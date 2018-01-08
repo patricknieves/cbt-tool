6bfd564fc01aba5536c3f078eccad195ee026493
Patrick Nieves <nieves_p@hotmail.de>
2018-01-08 22:18:24 +0100

    Added Tor authentification

--- main/Tor.py
+++ main/Tor.py
@@ -4,6 +4,7 @@ from stem.control import Controller
 
 def change_ip():
     with Controller.from_port(port=9051) as controller:
-        controller.authenticate()
+        controller.authenticate(password="16:872860B76453A77D60CA2BB8C1A7042072093276A3D701AD684053EC4C")
+        #controller.authenticate()
         controller.signal(Signal.NEWNYM)
 
