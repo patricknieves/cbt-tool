642f14e5b74b8d31dca4486b8d8f5dd4d673a6b7
Patrick Nieves <nieves_p@hotmail.de>
2018-01-23 18:00:02 +0100

    Bugfix Server import

--- main/scraper/Finder.py
+++ main/scraper/Finder.py
@@ -1,7 +1,9 @@
 import time
-from Data_retriever import Data_retriever
+import sys
+import os.path
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from main import Database_manager
-
+from Data_retriever import Data_retriever
 
 def find():
     #setup_db()
