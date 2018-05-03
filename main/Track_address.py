2627ddb588c6395615b9d9903d4a377ddf08c708
Patrick Nieves <nieves_p@hotmail.de>
2018-05-03 16:00:27 +0200

    Added documentation 2

--- main/Track_address.py
+++ main/Track_address.py
@@ -3,15 +3,15 @@ import sys
 
 
 def main(address_to_prove):
+    """ Track from where an address originated. Goes back the path taken by the address recognition """
     address_to_search = str(address_to_prove)
-    print("strart with " + address_to_search)
+    print("start with " + address_to_search)
     Database_manager.initialize_db()
     rels = Database_manager.get_relations(address_to_search)
     while rels:
         curr_addr = rels[0][2]
         print("From: INPUT:" + str(rels[0][1]) + ", OUTPUT " + str(rels[0][2]) + ", HASH: " + str(rels[0][3]))
         rels = Database_manager.get_relations(curr_addr)
-    print("finish")
 
 if __name__ == "__main__": main(sys.argv[1])
 
