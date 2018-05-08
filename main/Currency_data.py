6962d57c2f99be103cb07169067d1e46b41a78c9
Patrick Nieves <nieves_p@hotmail.de>
2018-05-08 15:34:38 +0200

    Bugfix Exchange rate

--- main/Currency_data.py
+++ main/Currency_data.py
@@ -14,21 +14,21 @@ class Currency_data(object):
 
     def get_value(self, transaction_time):
         """ Returns the value of a cryptocurrency for a given time in Dollar """
-        transaction_time = calendar.timegm(transaction_time.timetuple())
-        if not self.currency_data:
+        transaction_timestamp = calendar.timegm(transaction_time.timetuple())
+        if not self.currency_data or len(self.currency_data) == 1:
             Tor.change_ip()
-            self.currency_data = self.h.histoHour(self.currency_from, self.currency_to, toTs=(transaction_time + 60 * 60), limit=2000)["Data"][::-1]
+            self.currency_data = self.h.histoHour(self.currency_from, self.currency_to, toTs=(transaction_timestamp + 60 * 60), limit=2000)["Data"][::-1]
         for data_set in list(self.currency_data):
             # Delete Data which is older than 1 hour
-            if (transaction_time - data_set["time"]) < -60*60:
+            if (transaction_timestamp - data_set["time"]) < -60*60:
                 self.currency_data.remove(data_set)
-                if not self.currency_data:
+                if not self.currency_data or len(self.currency_data) == 1:
                     return self.get_value(transaction_time)
             # Ignore Data which is older but still not older than 1 hour
-            elif (transaction_time - data_set["time"]) < 0:
+            elif (transaction_timestamp - data_set["time"]) < 0:
                 continue
             # Return rate if data set is in range of 1 hour
-            elif (transaction_time - data_set["time"]) <= 60*60:
+            elif (transaction_timestamp - data_set["time"]) <= 60*60:
                 return max(data_set["low"], data_set["high"]) - abs(data_set["low"] - data_set["high"])/2
             else:
                 print ("No currency rate data found for Exchange from " + self.currency_from + " to " + self.currency_to)
